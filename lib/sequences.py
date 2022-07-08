5d3ae76c56342d65962e62d93c123213ecc1f581
github username <github email address>
2022-07-08 10:19:36 -0400

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = [0,1]
+    while len(fib) < length and length > 2:
+        fib.append(fib[-1] + fib[-2])
+
+    for x in range(0,length):
+        print(fib[x])
